b74fbcfbd7e0c7fa7cc4c61e94b3e20eee7ab50d
Geneviève Ernould <gen_ernould@hotmail.com>
2025-06-06 13:44:22 +0200

    hahaaha

--- new_test.py
+++ new_test.py
@@ -1,2 +1,3 @@
-print("hello again, wooooorld!!!!!!!!!!!!!!!!")
-print("je ne sais pas quoi écrire de plus pour ce test haha")
+print("hello again, wooooorld!")
+print("je ne sais pas quoi écrire...")
+print("bouuuuh")
